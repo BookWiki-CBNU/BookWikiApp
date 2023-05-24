f8273eaef6042cde9bc6a024db05d5d4ae8cb6cf
JeongSG <jsk41755@naver.com>
2023-05-25 04:27:18 +0900

    검색 기능 적용 완료. +북마크 적용시 메인 페이지 이전 까지 종료. 현재 전부 기능 구현 완료.

--- app/src/main/java/com/devjeong/bookwiki_cbnu/Retrofit/BookApiService.kt
+++ app/src/main/java/com/devjeong/bookwiki_cbnu/Retrofit/BookApiService.kt
@@ -6,6 +6,7 @@ import com.devjeong.bookwiki_cbnu.Model.BookResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BookApiService {
     @GET("book/read/{category}")
@@ -19,4 +20,11 @@ interface BookApiService {
 
     @GET("book/read/detail/{docId}")
     suspend fun getBookDetail(@Path("docId") docId: String): Response<BookDetailResponse>
+
+    @GET("book/read/bookNameSearch")
+    suspend fun searchBooks(
+        @Query("bookName") bookName: String,
+        @Query("category") category: String
+    ): BookResponse
+
 }
--- app/src/main/java/com/devjeong/bookwiki_cbnu/View/BookListActivity.kt
+++ app/src/main/java/com/devjeong/bookwiki_cbnu/View/BookListActivity.kt
@@ -3,6 +3,10 @@ package com.devjeong.bookwiki_cbnu.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.devjeong.bookwiki_cbnu.Adapter.BookListRecyclerAdapter
@@ -15,6 +19,7 @@ import kotlinx.coroutines.*
 class BookListActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var bookAdapter: BookListRecyclerAdapter
+    private lateinit var searchEt : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,7 @@ class BookListActivity : AppCompatActivity() {
         Log.d("category", category.toString())
 
         recyclerView = findViewById(R.id.bookRecyclerView)
+        searchEt = findViewById(R.id.searchEt)
         recyclerView.layoutManager = LinearLayoutManager(this)
         bookAdapter = BookListRecyclerAdapter(emptyList()) // 빈 리스트로 초기화
         recyclerView.adapter = bookAdapter
@@ -40,7 +46,59 @@ class BookListActivity : AppCompatActivity() {
                 fetchDataFromApi(bookCategory)
             }
         }
+
+        ///검색 기능 구현중///
+        searchEt.setOnKeyListener { v, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                val keyword = searchEt.text.toString().trim()
+                if (keyword.isNotEmpty()) {
+                    val bookCategory = when (category) {
+                        "Technology" -> "technology"
+                        "Art" -> "art"
+                        "Other" -> "other"
+                        "Social" -> "social"
+                        else -> null
+                    }
+                    Log.d("keyword", "$keyword $category")
+                    searchBooks(keyword, bookCategory!!)
+                } else {
+
+                }
+                return@setOnKeyListener true
+            }
+            false
+        }
     }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+
+    private fun searchBooks(keyword: String, category: String) {
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val response = RetrofitClient.bookApiService.searchBooks(keyword, category)
+                if (response.list.isNullOrEmpty()) {
+                    // 검색 결과가 없는 경우 처리
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(applicationContext,"검색한 요청의 책이 없습니다.", Toast.LENGTH_LONG).show()
+                    }
+                } else {
+                    val books = response.list
+                    withContext(Dispatchers.Main) {
+                        bookAdapter.updateData(books)
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e("searchBooks", "Error: ${e.message}")
+                withContext(Dispatchers.Main) {
+                    // TODO: 예외 처리 로직 구현
+                }
+            }
+        }
+    }
+
 
     private fun fetchDataFromApi(category: BookCategory) {
         GlobalScope.launch(Dispatchers.IO) {
